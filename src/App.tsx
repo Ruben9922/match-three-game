c773478ad9cc0d2d68a121165080e7cc26dffc25
Ruben9922 <Ruben9922@users.noreply.github.com>
2022-04-16 19:38:30 +0100

    Remove Konva, use SVG & Framer Motion, and update packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect} from "react";
-import {Layer, Rect, Stage} from "react-konva";
 import {useImmer} from "use-immer";
 import {generateGrid, refreshGrid, symbolCount} from "./game";
 import * as R from "ramda";
-import Konva from "konva";
+import { motion } from "framer-motion";
+import randomColor from "randomcolor";
 
-const symbolColors: string[] = R.times(Konva.Util.getRandomColor, symbolCount);
+const symbolColors: string[] = R.times(() => randomColor({ luminosity: "bright" }), symbolCount);
 
 function App() {
   const [grid, setGrid] = useImmer(generateGrid);
@@ -19,20 +19,17 @@ function App() {
   }, [setGrid]);
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        {grid.map((row, rowIndex) => row.map((symbol, cellIndex) => symbol !== null && (
-          <Rect
-            key={`${rowIndex},${cellIndex}`}
-            x={cellIndex * 70}
-            y={rowIndex * 70}
-            width={50}
-            height={50}
-            fill={symbolColors[symbol]}
-          />
-        )))}
-      </Layer>
-    </Stage>
+    <motion.svg width="1000" height="1000">
+      {grid.map((row, rowIndex) => row.map((symbol, cellIndex) => symbol !== null && (
+        <motion.circle
+          key={`${rowIndex},${cellIndex}`}
+          cx={(cellIndex * 70) + 25}
+          cy={(rowIndex * 70)+25}
+          r={25}
+          fill={symbolColors[symbol]}
+        />
+      )))}
+    </motion.svg>
   );
 }
 
